25dd8bd620a4ccf4dc56b41587e83d0869ed6181
unknown <duongtrung1910@gmail.com>
2019-11-29 10:13:25 +0700

    Update ahb2apb_core.sv

--- rtl/ahb2apb_core/ahb2apb_core.sv
+++ rtl/ahb2apb_core/ahb2apb_core.sv
@@ -54,6 +54,9 @@ parameter SLV_NUM = 4'd8;
 parameter AHB_OFFSET = 18'b0000_0000_0000_0000_00; 
 
 
+parameter IDLE    = 2'b00;
+parameter SETUP   = 2'b01;
+parameter ACCESS  = 2'b10;
 //------------------------------------------------------------------------------
 // Port declarations
 input logic                             ahb_clk;
@@ -114,10 +117,6 @@ logic                                   rdata_ff_ren;
 logic [31:0]                            rdata_ff_rdata;
 logic                                   rdata_ff_empty;
 
-logic                                   nxt_cmd_ff_ren;
-logic                                   cmd_ff_ren_1;
-logic                                   cmd_ff_ren_2;
-logic                                   cmd_ff_ren_3;
 logic [46:0]                            nxt_cmd;
 logic [46:0]                            cmd;
 logic [7:0]                             sel_apb;
@@ -126,6 +125,16 @@ logic                                   trans_done;
 logic                                   nxt_pen;
 logic                                   nxt_rdata_ff_ren;
 logic                                   nxt_hreadyout;
+logic                                   hreadyout;
+
+logic [1:0]                             nxt_apb_st;
+logic [1:0]                             apb_st;
+logic                                   idle_2_setup;
+logic                                   access_2_setup;
+logic                                   access_2_idle;
+logic                                   st_idle;
+logic                                   st_access;
+
 
 //------------------------------------------------------------------------------
 //Checking transfer is valid 
@@ -145,12 +154,12 @@ assign check_offset = (haddr[31:14] == AHB_OFFSET);
 //------------------------------------------------------------------------------
 //HREADYOUT control 
 assign hresp_o = 1'b0;
-assign nxt_hreadyout = ((~hwrite_i) & vld)  ? 1'b0 :
-                       rdata_ff_ren ? 1'b1 : hreadyout_o;
+assign nxt_hreadyout = ((~hwrite_i) & vld) ? 1'b0 :
+                       rdata_ff_ren ? 1'b1 : hreadyout;
                        
-dffa_rstn  #(1,1'b1)  dff_hreadyout (ahb_clk,rst_n,nxt_hreadyout,hreadyout_o);
+dffa_rstn  #(1,1'b1)  dff_hreadyout (ahb_clk,rst_n,nxt_hreadyout,hreadyout);
 
-
+assign hreadyout_o = hreadyout & (~cmd_ff_full);
 //------------------------------------------------------------------------------
 //Asynchronous command FIFO
 assign cmd_ff_wen = vld_1 & (~cmd_ff_full) & check_offset;
@@ -159,38 +168,67 @@ assign cmd_ff_wdata = {wen,hwdata_i[31:0],haddr[13:0]}; //47 bits
 ahb2apb_async_fifo  #(.AW(2),
                       .DW(47))  async_cmd_fifo  
     (
-    .rst_n   (rst_n),
+    .rst_n   (rst_n)       ,
     //-----------------------------
     //Write
-    .wclk    (ahb_clk),//Write clock
-    .wfifo_i (cmd_ff_wen),//Request writing FIFO
+    .wclk    (ahb_clk)     ,//Write clock
+    .wfifo_i (cmd_ff_wen)  ,//Request writing FIFO
     .wdata_i (cmd_ff_wdata),
-    .wfull_o (cmd_ff_full),
+    .wfull_o (cmd_ff_full) ,
     //-----------------------------
     //Read
-    .rclk    (apb_clk),//Read clock
-    .rfifo_i (cmd_ff_ren),
+    .rclk    (apb_clk)     ,//Read clock
+    .rfifo_i (cmd_ff_ren)  ,
     .rempty_o(cmd_ff_empty),
     .rdata_o (cmd_ff_rdata)
     );
 
 //------------------------------------------------------------------------------
-//Reading cmd fifo at APB site
+//APB master state machine
 
-assign nxt_cmd_ff_ren = (~cmd_ff_empty) & (~penable_o) & (~cmd_ff_ren) & 
-                        (~cmd_ff_ren_1) & (~cmd_ff_ren_2) & (~cmd_ff_ren_3);
+assign idle_2_setup   = (~cmd_ff_empty) & st_idle;
+assign access_2_setup = pready_i & (~cmd_ff_empty) & st_access;
+assign access_2_idle  = pready_i & cmd_ff_empty & st_access;
 
-dffa_rstn  #(1)  dff_cmd_ff_ren (apb_clk,rst_n,nxt_cmd_ff_ren,cmd_ff_ren);
-dffa_rstn  #(1)  dff_cmd_ff_ren_1 (apb_clk,rst_n,cmd_ff_ren,cmd_ff_ren_1);
-dffa_rstn  #(1)  dff_cmd_ff_ren_2 (apb_clk,rst_n,cmd_ff_ren_1,cmd_ff_ren_2);
-dffa_rstn  #(1)  dff_cmd_ff_ren_3 (apb_clk,rst_n,cmd_ff_ren_2,cmd_ff_ren_3);
 
-//Latching output data of command fifo 
-assign nxt_cmd = cmd_ff_ren_1 ? cmd_ff_rdata : cmd;
+always @ (*)
+    begin
+    case (apb_st)
+    	IDLE :
+            begin
+    	    nxt_apb_st = idle_2_setup ? SETUP : IDLE;
+    	    end	
+    	SETUP :
+    	    begin
+    	    nxt_apb_st = ACCESS;
+    	    end	
+    	ACCESS :
+    	    begin
+    	    nxt_apb_st = access_2_setup ? SETUP :
+                         access_2_idle  ? IDLE  : ACCESS;
+    	    end	
+    	default :
+    	    begin
+    	    nxt_apb_st = IDLE;
+    	    end	
+    endcase
+    end
+
+dffa_rstn #(2) dff_apb_st (apb_clk,rst_n,nxt_apb_st,apb_st);
+
+assign st_idle   = (apb_st == IDLE);
+assign st_access = (apb_st == ACCESS);
+//------------------------------------------------------------------------------
+//Reading command FIFO
+assign cmd_ff_ren = idle_2_setup | access_2_setup;
+
+
+//Latching output data of command fifo
+assign nxt_cmd = cmd_ff_ren ? cmd_ff_rdata : cmd;
 
 dffa_rstn  #(47)  dff_cmd_rdata (apb_clk,rst_n,nxt_cmd,cmd);
 
-//Decode
+//Adress decode
 assign sel_apb[0] = (cmd[13:11] == 3'b000);  
 assign sel_apb[1] = (cmd[13:11] == 3'b001);  
 assign sel_apb[2] = (cmd[13:11] == 3'b010);  
@@ -202,48 +240,23 @@ assign sel_apb[7] = (cmd[13:11] == 3'b111);
 
 //------------------------------------------------------------------------------
 //PSEL0-7 control
-assign trans_done  = penable_o & (|(pready_i & sel_apb));
-assign nxt_psel[0] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[0]) ? 1'b1 : psel_o[0];
 
-assign nxt_psel[1] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[1]) ? 1'b1 : psel_o[1];
-
-assign nxt_psel[2] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[2]) ? 1'b1 : psel_o[2];
-
-assign nxt_psel[3] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[3]) ? 1'b1 : psel_o[3];
-
-assign nxt_psel[4] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[4]) ? 1'b1 : psel_o[4];
-
-assign nxt_psel[5] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[5]) ? 1'b1 : psel_o[5];
-
-assign nxt_psel[6] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[6]) ? 1'b1 : psel_o[6];
-
-assign nxt_psel[7] = trans_done ? 1'b0 :
-                     (cmd_ff_ren_2 & sel_apb[7]) ? 1'b1 : psel_o[7];
-
-
-dffa_rstn  #(8)  dff_psel (apb_clk,rst_n,nxt_psel,psel_o);
+assign psel_o = sel_apb;
+//dffa_rstn  #(8)  dff_psel (apb_clk,rst_n,nxt_psel,psel_o);
 
 //------------------------------------------------------------------------------
 //PENABLE control
-assign nxt_pen = trans_done   ? 1'b0 :
-                 cmd_ff_ren_3 ? 1'b1 : penable_o;
-                  
-
+assign nxt_pen = st_access; 
 dffa_rstn  #(1)  dff_pen (apb_clk,rst_n,nxt_pen,penable_o);
 //------------------------------------------------------------------------------
 //PADDR control
-dffa_rstn  #(11)  dff_paddr (apb_clk,rst_n,cmd[10:0],paddr_o);
+//dffa_rstn  #(11)  dff_paddr (apb_clk,rst_n,cmd[10:0],paddr_o);
+assign paddr_o = cmd[10:0];
 
 //------------------------------------------------------------------------------
 //PWDATA control
-dffa_rstn  #(32)  dff_pwdata (apb_clk,rst_n,cmd[45:14],pwdata_o);
+//dffa_rstn  #(32)  dff_pwdata (apb_clk,rst_n,cmd[45:14],pwdata_o);
+assign pwdata_o = cmd[45:14];
 
 //------------------------------------------------------------------------------
 //PSTRB control
@@ -251,11 +264,13 @@ assign pstrb_o = 4'b1111;
 
 //------------------------------------------------------------------------------
 //PWRITE control
-dffa_rstn  #(1)  dff_pwrite (apb_clk,rst_n,cmd[46],pwrite_o);
-
+//dffa_rstn  #(1)  dff_pwrite (apb_clk,rst_n,cmd[46],pwrite_o);
+assign pwrite_o = cmd[46];
 //------------------------------------------------------------------------------
 //Control writing to read data fifo
-assign rdata_ff_wen = (~rdata_ff_full) & trans_done & (~pwrite_o);
+assign rdata_ff_wen = (~rdata_ff_full) & (access_2_setup | access_2_idle) & 
+                      (~pwrite_o);
+
 assign rdata_ff_wdata = sel_apb[0] ? prdata_i[0] :
                         sel_apb[1] ? prdata_i[1] :
                         sel_apb[2] ? prdata_i[2] :
